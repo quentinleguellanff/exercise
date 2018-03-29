ff8b5d7eb0726d89cf0b6fa95ac564dbb2903977
quentinleguellanff <37702517+quentinleguellanff@users.noreply.github.com>
2018-03-30 01:43:59 +0200

    Add files via upload

---
+++ checkerboard_other.py
@@ -0,0 +1,12 @@
+square_nb = 64;
+last_square = 0;
+rice_sum = 0;
+common_ratio = 2;
+
+last_square = common_ratio**square_nb;
+rice_sum = (last_square - 1)/(common_ratio-1);
+
+print rice_sum
+
+
+
